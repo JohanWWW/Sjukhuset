234e6f37a7a1adfce101794cd3f99d755d22f0d9
DESKTOP-6MAAUG9\Hämid <hamid.nazarifar@hotmail.com>
2020-09-22 16:28:43 +0200

    initial commit

---
+++ src/com/company/Patient.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Patient {
+    private String name;
+    private String diagnosis;
+
+    public Patient(String name, String diagnosis) {
+
+    }
+    public Patient(String name) {
+
+    }
+}
+
