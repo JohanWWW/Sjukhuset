2b6a13eca6f4fa25c1fb3b8585cae574370cf251
JohanWWW <42637697+JohanWWW@users.noreply.github.com>
2020-10-01 19:50:56 +0200

    change access modifiers

--- src/com/company/Patient.java
+++ src/com/company/Patient.java
@@ -1,32 +1,32 @@
 package com.company;
 
-public class Patient {
+class Patient {
     private String name;
     private String diagnosis; // Null means the patient is healthy
 
-    public Patient(String name, String diagnosis) {
+    Patient(String name, String diagnosis) {
         this.name = name;
         this.diagnosis = diagnosis;
     }
 
-    public Patient(String name) {
+    Patient(String name) {
         this.name = name;
         this.diagnosis = null;
     }
 
-    public boolean isSick() {
+    boolean isSick() {
         return (diagnosis != null);
     }
 
-    public String getSickness() {
+    String getSickness() {
         return diagnosis;
     }
 
-    public String getName() {
+    String getName() {
         return name;
     }
 
-    public void takeMedication(Medicine medicine) {
+    void takeMedication(Medicine medicine) {
         if (medicine.getTreatmentName().equals(getSickness())) {
             diagnosis = null;
         }
